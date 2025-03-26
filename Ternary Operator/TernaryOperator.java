ce5bf9bc22a7800bc9a8290b217cb4c033289f7d
Danish Ali <danishalikango1@gmail.com>
2025-03-26 23:08:47 +0500

    Add files via upload

---
+++ Ternary Operator/TernaryOperator.java
@@ -0,0 +1,16 @@
+package javafundamentals;
+
+public class TernaryOperator {
+ 
+    public static void main(String[] args) {
+        int num1 = 30;
+        int num2 = 40;
+        
+        int maxNum = (num1>num2) ? num1 : num2;
+        
+        System.out.println("Maximium is: " + maxNum);
+        
+        
+        
+    }
+}
